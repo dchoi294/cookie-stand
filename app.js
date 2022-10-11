e74ac3d89e9fc11e3a0fd1d0a7095dc55d59e4d3
Don Choi <dchoi294@gmail.com>
2022-10-11 02:58:43 +0000

    progress of lab6

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let li = document.createElement('li');
+li.textContent = `6am: ${seattle.someValueInArray[i]} cookies`;
+
+let seattle = {
+  name:'Seattle',
+  soldCookies: [16,20,35,48,56,77,93,144,119,84,61,23,42,57],
+  render: function() {
+    for (let i = 0; i < this.soldCookies.length; i++) {
+      let 
+    }
+  }
+}
